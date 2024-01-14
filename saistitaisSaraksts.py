1428264064b91a2eaa4470d5b24d9a53f9363bfd
katrine <92726236+andromeda44azora@users.noreply.github.com>
2024-01-14 23:54:05 +0200

    aaasdsda
    sdadsdada

---
+++ saistitaisSaraksts.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, saturs, pirms=None, pec=None):
+        self.info = saturs
+        self.prev = pirms
+        self.next = pec
+
+    def read(self):
+        print(self.info)
+
+class List:
+    def __init__(self, pirmais_info):
+        self.pirmais = Node(pirmais_info)
+    
+    def add(self, jaunais_info, indekss = -1):
+        if indekss == -1:
+            pedejais = self.pirmais
+            while pedejais.next:
+                pedejais = pedejais.next
+            # pedejais.next = Node(jaunais_info, pirms = pedejais)
+            pedejais.next = Node(jaunais_info)
+            pedejais.next.prev = pedejais
+            return pedejais.next
+        
+        if indekss == 0:
+            self.pirmais = Node(jaunais_info, pec=self.pirmais)
+            return self.pirmais
+        
+        ieprieksejais = self.pirmais
+        for i in range(indekss-1):
+            if ieprieksejais.next == None:
+                return self.add(jaunais_info)
+            ieprieksejais = ieprieksejais.next
+           
+        ieprieksejais.next = Node(jaunais_info, pec = ieprieksejais.next, pirms=ieprieksejais)
+        
+    
+    def read(self):
+        esosais = self.pirmais
+        while esosais:
+            esosais.read()
+            esosais = esosais.next
+
+saraksts = List("suns")
+saraksts.add(24)
+saraksts.add("hei, visi!")
+saraksts.add("pirmais", 0)
+saraksts.add("ceturtais", 3)
+saraksts.add("beigas",33)
+saraksts.read()
+
+class SaistītaisSaraksts:
+    def __init__(self):
+        self.saraksts = []
+
+    def pievienot_elementu(self, elements):
+        self.saraksts.append(elements)
+
+    def dzest_elementu(self, indekss):
+        if 0 <= indekss < len(self.saraksts):
+            del self.saraksts[indekss]
+            print(f"Elements ar indeksu {indekss} ir veiksmīgi dzēsts.")
+        else:
+            print("Nederīgs indekss. Dzēšana neizdevās.")
+
+    def nolasit_elementu(self, indekss):
+        if 0 <= indekss < len(self.saraksts):
+            print(f"Elements ar indeksu {indekss}: {self.saraksts[indekss]}")
+        else:
+            print("Nederīgs indekss. Nolasīšana neizdevās.")
